590b1fc7ff07d63da5e2d1c7b8637e4ed3c8c9d8
Jackson Sprague <jtsmoviemaker@icloud.com>
2025-11-08 21:08:52 -0500

    WAVE

--- gameoff2025/Nodes/big_wave.gd
+++ gameoff2025/Nodes/big_wave.gd
@@ -1,15 +1,36 @@
 extends Node2D
 class_name BigWave
+var go = false
+var speed = 3000
+var dir = 1
+var StartPoint=0000
+var INTERPFACTOR = 0
+var OwnerRef :GameManager
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	OwnerRef=get_parent()
+	StartPoint=position.y
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	position.y=3500*$INTERP.rotation_degrees
+
+
 
 
 func _Crash():
-	$AnimationPlayer.play("WaveCrash")
+	$WaveCore/AnimationPlayer.play("INTERP")
+
+
+func Receed():
+	$WaveCore/AnimationPlayer.play("Receed")
+
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name=="INTERP":
+		OwnerRef.PowerUps()
+	if anim_name=="Receed":
+		OwnerRef.AwaitNextWave()
--- gameoff2025/Nodes/game_manager.gd
+++ gameoff2025/Nodes/game_manager.gd
@@ -1,4 +1,6 @@
 extends Node2D
+class_name GameManager
+
 @export var PlayerRef :Player
 
 var Difficulty = 1
@@ -13,13 +15,31 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	$Warning.position.x=PlayerRef.position.x
+	$Warning.position.y=PlayerRef.position.y+200
+	
 
+
+func PowerUps():
+	print("Power!")
+	BigWaveRef.Receed()
+
+func AwaitNextWave():
+	print("waiting")
+	BigWaveRef.queue_free()
+	$Wave_Frequency.start()
 
 func _on_wave_frequency_timeout() -> void:
+	$Warning.visible=true
+	$Countdown_timer.start()
+
+
+
+func _on_countdown_timer_timeout() -> void:
+	$Warning.visible=false
 	BigWaveRef = WaveScene.instantiate()
 	add_child(BigWaveRef)
-	BigWaveRef.global_position.x=PlayerRef.global_position.x
-	BigWaveRef.global_position.y=PlayerRef.global_position.y-2500
+	BigWaveRef.global_position.x=PlayerRef.position.x
+	BigWaveRef.global_position.y=3500
 	BigWaveRef._Crash()
 	print("CRASSSH")

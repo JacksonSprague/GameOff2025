2f1b635a2064abe15535bad58a3373ba7eca7a52
LightningTorn8do <154620770+LightningTorn8do@users.noreply.github.com>
2025-11-09 23:13:31 -0500

    enemy

--- gameoff2025/Scenes/base.gd
+++ gameoff2025/Scenes/base.gd
@@ -5,3 +5,16 @@ class_name CharacterBase
 
 @onready var visuals: Node2D = %Visuals
 @onready var animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
+@onready var health_component: HealthComponent = $HealthComponent
+
+
+func _ready() -> void:
+	health_component.setup(stats)
+
+
+func _on_hurtbox_component_on_damaged(hitbox: HitboxComponent) -> void:
+	if health_component.current_health <= 0:
+		return
+	
+	health_component.take_damage(hitbox.damage)
+	print("%s: %d" % [name, health_component.current_health])

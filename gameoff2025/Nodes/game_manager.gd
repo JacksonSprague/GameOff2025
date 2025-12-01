054405065e76ea6ba1cb892650adc7d00aca9888
LightningTorn8do <154620770+LightningTorn8do@users.noreply.github.com>
2025-12-01 02:28:41 -0500

    a

--- gameoff2025/Nodes/game_manager.gd
+++ gameoff2025/Nodes/game_manager.gd
@@ -21,7 +21,7 @@ var isWave =false
 var ability_dictionary = {
 	"a_Attack": {"weight" : 1.5, "scene" : preload("res://Abilities/Scenes/a_Attack.tscn"), "desc" : "Increase attack power by 10%"},
 	"a_Health": {"weight" : 1.5, "scene" : preload("res://Abilities/Scenes/a_Health.tscn"), "desc" : "Increase maximum health by 30%"},
-	"a_Speed": {"weight" : 1.5, "scene" : preload("res://Abilities/Scenes/a_Speed.tscn"), "desc" : "Roam the beach 10% faster"},
+	"a_Speed": {"weight" : 2.0, "scene" : preload("res://Abilities/Scenes/a_Speed.tscn"), "desc" : "Roam the beach 10% faster"},
 	"a_Shell Strength": {"weight" : 1.5, "scene" : preload("res://Abilities/Scenes/a_Shell Strength.tscn"), "desc" : "Reduce shell strength needed to burrow by 10%"},
 	"a_Small Shot": {"weight" : 1, "scene" : preload("res://Abilities/Scenes/a_Small Shot.tscn"), "desc" : "Gain or enhance the SMALL SHOT weapon which fires small but deadly projectiles"},
 	"a_Magnet": {"weight" : 2, "scene" : preload("res://Abilities/Scenes/a_Magnet.tscn"), "desc" : "Increase the distance from which you can grab shell shards by 30%"},

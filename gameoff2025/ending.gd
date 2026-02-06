0a778b8c98962f927717edf859f70e40f7e54f7e
LightningTorn8do <154620770+LightningTorn8do@users.noreply.github.com>
2026-02-05 23:01:30 -0500

    a

--- gameoff2025/ending.gd
+++ gameoff2025/ending.gd
@@ -3,4 +3,4 @@ extends Area2D
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Player"):
-		get_tree().change_scene_to_file("res://Scenes/Victory.tscn")
+		get_tree().change_scene_to_file("uid://dgw4wbrkdw4m")
